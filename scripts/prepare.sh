a12118fe2458b7f9e10eaf9a141f153c705a7975
Cameron Johnston <cameron@rootdown.net>
2016-12-14 14:33:20 -0700

    use rc.local to force clock update at boot

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -9,3 +9,9 @@ echo 'mkdir /home/centos/.ssh' >> /etc/rc.local
 echo 'curl -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key > /home/centos/.ssh/authorized_keys' >> /etc/rc.local
 echo 'chmod 600 /home/centos/.ssh/authorized_keys' >> /etc/rc.local
 echo 'chown -R centos.centos /home/centos/.ssh' >> /etc/rc.local
+echo '( /etc/init.d/ntpd stop
+until ping -nq -c3 8.8.8.8; do
+   echo "Waiting for network..."
+done
+ntpdate -s time.nist.gov
+/etc/init.d/ntpd start )&' >> /etc/rc.local
