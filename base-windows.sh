ca1d01f711d6af98896c4d317096603a54ac0701
Greg Cymbalski <gcymbalski@sensu.io>
2017-04-05 13:12:25 -0700

    breaking out 2008 and 2012 into separate templates for fixups

--- base-windows.sh
+++ base-windows.sh
@@ -9,8 +9,8 @@ else
 fi
 
 # XXX TODO lookup image based on region- right now everything is hardcoded for us-west-2
-# current AMI is windows-2012r2 updated 03/15/2017
-AMI=ami-09f47d69
 GITREV=`git describe --all |sed -e 's/\//-/g'`
 
-PACKER_LOG=1 packer build ${DEBUGSNIP} --var git_rev=${GITREV} --var source_ami=${AMI} base-windows.json 
+for i in 2008 2012; do
+  PACKER_LOG=1 packer build ${DEBUGSNIP} --var git_rev=${GITREV} --var source_ami=${AMI} base-windows-${i}.json 
+end
