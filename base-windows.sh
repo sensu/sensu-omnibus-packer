f2c356f61b2d1087f3c4cc37482dc46a98e16fa4
Greg Cymbalski <gcymbalski@sensu.io>
2017-04-05 13:15:27 -0700

    for now we can keep the base ami in the json

--- base-windows.sh
+++ base-windows.sh
@@ -12,5 +12,5 @@ fi
 GITREV=`git describe --all |sed -e 's/\//-/g'`
 
 for i in 2008 2012; do
-  PACKER_LOG=1 packer build ${DEBUGSNIP} --var git_rev=${GITREV} --var source_ami=${AMI} base-windows-${i}.json 
+  PACKER_LOG=1 packer build ${DEBUGSNIP} --var git_rev=${GITREV} base-windows-${i}.json 
 end
